e07e2e4b74cdde2d1d0be0f6a1c294195c9a0962
Michał <49103449+jitoni@users.noreply.github.com>
2025-08-16 20:15:43 +0200

    update 16.08.2025
    new video, changes in about-me

--- script.js
+++ script.js
@@ -73,3 +73,46 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 });
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  // Select the button element
+  var videos = document.querySelectorAll('.video-modal-big');
+
+  // Loop through each video element and bind a click event listener to it
+  videos.forEach(function(video) {
+    video.addEventListener('click', function() {
+
+      // Get the video URL from the data-video-id attribute
+      var ytUrl = this.getAttribute('ytSrc');
+      var ytCaption = this.getAttribute('yt-caption');
+
+      // Create the HTML for the modal, including the video URL
+      var modalHtml = '<div class="card-box-video">' +
+                      '<div class="modal-content" oncontextmenu="return false;" ondragstart="return false;">' +
+                      '<iframe width="760" height="515" src="' + ytUrl + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>' +
+                      '<div id="caption" class="caption">' + ytCaption + '</div>' +
+                      '</div>' +
+                      '</div>';
+
+      // Create a new element for the modal and add the HTML to it
+      var modal = document.createElement("div");
+      modal.innerHTML = modalHtml;
+
+      var caption = document.getElementById("caption");
+      caption.innerHTML = this.alt;
+      
+      // document.body.insertBefore(modal, box);
+
+      // Append the modal to the body
+      document.body.appendChild(modal);
+
+      // Select the close button and bind a click event listener to it
+      var closeButton = modal.querySelector('.modal-content');
+      closeButton.addEventListener('click', function() {
+        // Remove the modal from the DOM
+        modal.remove();
+      });
+    });
+  });
+});
